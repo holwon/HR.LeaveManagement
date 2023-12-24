8a4cacb6de1d91657c6c116a3b9e05b908f10f84
holwon <holvon@outlook.com>
2023-12-24 15:55:15 +0800

    添加MediatR

--- HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
+++ HR.LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -6,11 +6,16 @@ namespace HR.LeaveManagement.Application;
 
 public static class ApplicationServicesRegistration
 {
-    public static void Registration(this IServiceCollection services)
+    public static IServiceCollection Registration(this IServiceCollection services)
     {
         // 这样子会遍历所有继承了 Profile 的类进行注入
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
         // services.AddAutoMapper(typeof(MappingProfile));
+
+        // services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Startup).Assembly));
+        services.AddMediatR(
+            cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+        );
+        return services;
     }
 }
